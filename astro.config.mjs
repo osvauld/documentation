529367d12cf1db20e144b61f14a7ae63aebb68e6
Abraham <abrahamgeorge8547@gmail.com>
2024-03-18 19:05:56 +0530

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
-
+import astroD2 from 'astro-d2'
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    astroD2(),
     starlight({
       title: "osvauld",
       customCss: ["./src/styles/custom.css"],
@@ -19,69 +20,82 @@ export default defineConfig({
           items: [
             { label: "Discovery FAQ", link: "/introduction/example" },
             { label: "Roadmap", link: "/introduction/roadmap/" },
-            { label: "Security", link: "/introduction/security/" },
-            { label: "Release notes", link: "/introduction/release-notes/" },
-            {
-              label: "Incident reports",
-              link: "/introduction/incident-reports/",
-            },
-            {
-              label: "Talk to a human",
-              link: "/introduction/talk-to-a-human/",
-            },
-          ],
+            // { label: "Security", link: "/introduction/security/" },
+            // { label: "Release notes", link: "/introduction/release-notes/" },
+            // {
+            //   label: "Incident reports",
+            //   link: "/introduction/incident-reports/",
+            // },
+            // {
+            //   label: "Talk to a human",
+            //   link: "/introduction/talk-to-a-human/",
+            // },
+          ], 
         },
+
         {
           label: "Installation",
           items: [
-            { label: "Debian 12", link: "/installation/debian-12/" },
-            { label: "Docker", link: "/installation/docker/" },
-            { label: "AWS AMI", link: "/installation/aws-ami/" },
-          ],
+            {label: "First User Creation", link: "/installation/first-user-creation/"},
+          ]
         },
         {
-          label: "Update",
+          label: "System Design",
           items: [
-            { label: "Update your osvauld instance", link: "/update/" },
-            { label: "Update for Ubuntu", link: "/update/ubuntu/" },
-            { label: "Update for Docker", link: "/update/docker/" },
-          ],
-        },
-        {
-          label: "Upgrade",
-          items: [
-            {
-              label: "Migrate to CentOS 7 package",
-              link: "/upgrade/centos-7-package/",
-            },
-            {
-              label: "Migrate to Debian package",
-              link: "/upgrade/debian-package/",
-            },
-            {
-              label: "Migrate to Ubuntu package",
-              link: "/upgrade/ubuntu-package/",
-            },
-          ],
-        },
-        {
-          label: "Backup",
-          items: [
-            { label: "Backup your osvauld instance", link: "/backup/" },
-            { label: "From source", link: "/backup/from-source/" },
-            { label: "DEB/RPM package", link: "/backup/deb-rpm-package/" },
-            { label: "Docker", link: "/backup/docker/" },
-          ],
-        },
-        {
-          label: "Installation Issues",
-          items: [
-            {
-              label: "Community forum",
-              link: "/installation-issues/community-forum/",
-            },
-          ],
-        },
+            {label: "Registration", link: "/system-design/registration/"},
+          ]
+        }
+        // {
+        //   label: "Installation",
+        //   items: [
+        //     { label: "Debian 12", link: "/installation/debian-12/" },
+        //     { label: "Docker", link: "/installation/docker/" },
+        //     { label: "AWS AMI", link: "/installation/aws-ami/" },
+        //   ],
+        // },
+        // {
+        //   label: "Update",
+        //   items: [
+        //     { label: "Update your osvauld instance", link: "/update/" },
+        //     { label: "Update for Ubuntu", link: "/update/ubuntu/" },
+        //     { label: "Update for Docker", link: "/update/docker/" },
+        //   ],
+        // },
+        // {
+        //   label: "Upgrade",
+        //   items: [
+        //     {
+        //       label: "Migrate to CentOS 7 package",
+        //       link: "/upgrade/centos-7-package/",
+        //     },
+        //     {
+        //       label: "Migrate to Debian package",
+        //       link: "/upgrade/debian-package/",
+        //     },
+        //     {
+        //       label: "Migrate to Ubuntu package",
+        //       link: "/upgrade/ubuntu-package/",
+        //     },
+        //   ],
+        // },
+        // {
+        //   label: "Backup",
+        //   items: [
+        //     { label: "Backup your osvauld instance", link: "/backup/" },
+        //     { label: "From source", link: "/backup/from-source/" },
+        //     { label: "DEB/RPM package", link: "/backup/deb-rpm-package/" },
+        //     { label: "Docker", link: "/backup/docker/" },
+        //   ],
+        // },
+        // {
+        //   label: "Installation Issues",
+        //   items: [
+        //     {
+        //       label: "Community forum",
+        //       link: "/installation-issues/community-forum/",
+        //     },
+        //   ],
+        // },
       ],
     }),
   ],
