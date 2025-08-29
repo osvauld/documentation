7535643f1c0dfd00114244642f6ed5a08920778c
tony <tonyantony300@gmail.com>
2025-08-29 19:18:43 +0530

    few ui changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,20 @@ import d2 from "astro-d2";
 export default defineConfig({
   integrations: [starlight({
     title: "Osvauld Documentation",
+    favicon: '/favicon.ico',
+    social: [
+      { icon: 'telegram', label: 'Telegram', href: 'https://t.me/+tjZLZK8JV8g0NTVl'},
+      {
+        icon: 'github',
+        label: 'GitHub',
+        href: 'https://github.com/osvauld/osvauld',
+      },
+      {
+        icon: 'email', 
+        label: 'Email',
+        href: 'mailto:abe@osvauld.com?subject=Hello Osvauld', 
+      }
+    ],
     customCss: ["./src/styles/custom.css"],
     sidebar: [
       {
@@ -34,6 +48,36 @@ export default defineConfig({
         items: [
           { label: "Setting Up ", link: "/sovereign-node/setting-up" },
         ],
+      },
+    ],
+    head: [
+      {
+        tag: 'meta',
+        attrs: {
+          property: 'og:image',
+          content: '/osvauldthumb.png',
+        },
+      },
+      {
+        tag: 'meta',
+        attrs: {
+          property: 'og:image:alt',
+          content: 'Osvauld Documentation Preview',
+        },
+      },
+      {
+        tag: 'meta',
+        attrs: {
+          name: 'twitter:card',
+          content: 'summary_large_image',
+        },
+      },
+      {
+        tag: 'meta',
+        attrs: {
+          name: 'twitter:image',
+          content: '/osvauldthumb.png',
+        },
       },
     ],
     plugins: [ion()],
